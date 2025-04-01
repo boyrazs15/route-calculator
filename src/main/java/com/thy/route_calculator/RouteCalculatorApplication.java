ef3492f0818e06f9f9f8d32da291843f2d5f0f9a
Sila Boyraz <sila.poyraz97@gmail.com>
2025-04-02 01:10:50 +0300

    initial commit

---
+++ src/main/java/com/thy/route_calculator/RouteCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.thy.route_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteCalculatorApplication.class, args);
+	}
+
+}
