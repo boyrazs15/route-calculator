55824c679d17e9feedff14a14cf89ff80d00f50e
Sila Boyraz <sila.poyraz97@gmail.com>
2025-04-07 21:56:46 +0300

    feat: add api key to swagger

--- backend/src/main/java/com/thy/route_calculator/config/SwaggerConfig.java
+++ backend/src/main/java/com/thy/route_calculator/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.thy.route_calculator.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +14,15 @@ public class SwaggerConfig {
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
-        .info(
-            new Info()
-                .title("Route Calculator API")
-                .version("1.0")
-                .description("CRUD endpoints for Location entity"));
+        .addSecurityItem(new SecurityRequirement().addList("apiKey"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "apiKey",
+                    new SecurityScheme()
+                        .name("X-API-KEY")
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)))
+        .info(new Info().title("Route Calculator API").version("1.0"));
   }
 }
