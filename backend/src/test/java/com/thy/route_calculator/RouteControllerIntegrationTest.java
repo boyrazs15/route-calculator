0da2aa3d8c16044c6318c5cefc62f3e5f2631108
Sila Boyraz <sila.poyraz97@gmail.com>
2025-04-07 09:45:08 +0300

    test: add additional test cases

--- backend/src/test/java/com/thy/route_calculator/RouteControllerIntegrationTest.java
+++ backend/src/test/java/com/thy/route_calculator/RouteControllerIntegrationTest.java
@@ -25,8 +25,6 @@ class RouteControllerIntegrationTest {
 
   private final RestTemplate restTemplate = new RestTemplate();
 
-  @Autowired private ObjectMapper objectMapper;
-
   private String buildQuery(Long origin, Long destination, LocalDate date) {
     return "?originLocationId="
         + origin
@@ -41,37 +39,134 @@ class RouteControllerIntegrationTest {
   }
 
   @Test
-  void shouldReturnValidRoutesForCorrectInput() {
-    String query = "?originLocationId=1&destinationLocationId=2&date=2025-04-07";
+  void shouldReturnValidRoutesForOnlyFlightRoute() {
+    // only flight
+    Long validOrigin = 6L; // Istanbul Airport
+    Long validDestination = 5L; // London Heathrow Airport
+    LocalDate validDate = LocalDate.of(2025, 4, 7);
+    String query = buildQuery(validOrigin, validDestination, validDate);
 
-    // Act
     ResponseEntity<List<RouteListingResponseDto>> response =
         restTemplate.exchange(
             buildUrl(query), HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
 
-    // Assert
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(response.getBody()).isNotNull();
-    assertThat(response.getBody().size()).isGreaterThan(0);
 
-    RouteListingResponseDto first = response.getBody().get(0);
-    assertThat(first.getFlight().getTransportationType()).isEqualTo(TransportationType.FLIGHT);
+    List<RouteListingResponseDto> routes = response.getBody();
+    assertThat(routes).isNotEmpty();
+    assertThat(routes.size()).isEqualTo(1);
+
+    RouteListingResponseDto flight = routes.get(0);
+    assertThat(flight.getAfterFlightTransfer()).isNull();
+    assertThat(flight.getBeforeFlightTransfer()).isNull();
+    assertThat(flight.getFlight().getTransportationType()).isEqualTo(TransportationType.FLIGHT);
+  }
+
+  @Test
+  void shouldReturnValidRoutesForAMultipleFlightRouteOnOperatingDate() {
+    // only flight
+    Long validOrigin = 4L; // Taksim Square
+    Long validDestination = 8L; // Wembley Stadium
+    LocalDate operatingDate = LocalDate.of(2025, 4, 7); // Monday
+    String query = buildQuery(validOrigin, validDestination, operatingDate);
+
+    ResponseEntity<List<RouteListingResponseDto>> response =
+        restTemplate.exchange(
+            buildUrl(query), HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    List<RouteListingResponseDto> routes = response.getBody();
+    assertThat(routes).isNotEmpty();
+    assertThat(routes.size()).isEqualTo(6);
+    assertThat(routes.toString())
+        .contains(
+            TransportationType.BUS.toString(),
+            TransportationType.FLIGHT.toString(),
+            TransportationType.UBER.toString(),
+            TransportationType.SUBWAY.toString());
+
+    RouteListingResponseDto firstRoute = routes.get(0);
+    assertThat(firstRoute.getBeforeFlightTransfer()).isNotNull();
+    assertThat(firstRoute.getBeforeFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.SUBWAY);
+    assertThat(firstRoute.getFlight().getOrigin()).isEqualTo("İstanbul Airport");
+    assertThat(firstRoute.getAfterFlightTransfer().getOrigin())
+        .isEqualTo("London Heathrow Airport");
+    assertThat(firstRoute.getAfterFlightTransfer().getDestination()).isEqualTo("Wembley Stadium");
+    assertThat(firstRoute.getAfterFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.UBER);
+
+    RouteListingResponseDto forthRoute = routes.get(5);
+    assertThat(forthRoute.getAfterFlightTransfer()).isNotNull();
+    assertThat(forthRoute.getBeforeFlightTransfer()).isNotNull();
+    assertThat(forthRoute.getFlight().getOrigin()).isEqualTo("Sabiha Gökçen Havalimanı");
+    assertThat(forthRoute.getBeforeFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.SUBWAY);
+    assertThat(forthRoute.getAfterFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.BUS);
+  }
+
+  @Test
+  void shouldReturnValidRoutesForAMultipleFlightRouteOnNonOperatingDate() {
+    // only flight
+    Long validOrigin = 4L; // Taksim Square
+    Long validDestination = 8L; // Wembley Stadium
+    LocalDate nonOperatingDate = LocalDate.of(2025, 4, 13); // Sunday
+    String query = buildQuery(validOrigin, validDestination, nonOperatingDate);
+
+    ResponseEntity<List<RouteListingResponseDto>> response =
+        restTemplate.exchange(
+            buildUrl(query), HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    List<RouteListingResponseDto> routes = response.getBody();
+    assertThat(routes).isNotEmpty();
+    assertThat(routes.size()).isEqualTo(3);
+    assertThat(routes.toString())
+        .contains(
+            TransportationType.FLIGHT.toString(),
+            TransportationType.UBER.toString(),
+            TransportationType.BUS.toString(),
+            TransportationType.SUBWAY.toString());
+
+    for (RouteListingResponseDto route : routes) {
+      assertThat(route.getAfterFlightTransfer().toString())
+          .doesNotContain(TransportationType.BUS.toString()); // non-operating day
+    }
+    RouteListingResponseDto firstRoute = routes.get(0);
+    assertThat(firstRoute.getBeforeFlightTransfer()).isNotNull();
+    assertThat(firstRoute.getBeforeFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.SUBWAY);
+    assertThat(firstRoute.getFlight().getOrigin()).isEqualTo("İstanbul Airport");
+    assertThat(firstRoute.getAfterFlightTransfer().getOrigin())
+        .isEqualTo("London Heathrow Airport");
+    assertThat(firstRoute.getAfterFlightTransfer().getDestination()).isEqualTo("Wembley Stadium");
+    assertThat(firstRoute.getAfterFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.UBER);
+
+    RouteListingResponseDto forthRoute = routes.get(2);
+    assertThat(forthRoute.getAfterFlightTransfer()).isNotNull();
+    assertThat(forthRoute.getBeforeFlightTransfer()).isNotNull();
+    assertThat(forthRoute.getFlight().getOrigin()).isEqualTo("Sabiha Gökçen Havalimanı");
+    assertThat(forthRoute.getBeforeFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.SUBWAY);
+    assertThat(forthRoute.getAfterFlightTransfer().getTransportationType())
+        .isEqualTo(TransportationType.UBER);
   }
 
   @Test
   void shouldReturn404WhenNoLocationFound() {
     Long invalidOrigin = 999L;
-    Long validDestination = 8L;
+    Long validDestination = 8L; // Wembley Stadium
     LocalDate invalidDate = LocalDate.of(2025, 4, 7);
     String query = buildQuery(invalidOrigin, validDestination, invalidDate);
 
-    // Act
     try {
-      // Act
       restTemplate.exchange(buildUrl(query), HttpMethod.GET, null, String.class);
       fail("Expected HttpClientErrorException.NotFound");
     } catch (HttpClientErrorException.NotFound ex) {
-      // Assert
       assertThat(ex.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
       assertThat(ex.getResponseBodyAsString()).contains(ErrorCode.LOCATION_NOT_FOUND.getCode());
       assertThat(ex.getResponseBodyAsString()).contains("Location not found with id: 999");
@@ -80,17 +175,15 @@ class RouteControllerIntegrationTest {
 
   @Test
   void shouldReturn404WhenNoFlightFound() {
-    Long origin = 5L;
-    Long destination = 8L;
+    Long origin = 5L; // London Heathrow Airport
+    Long destination = 8L; // Wembley Stadium
     LocalDate invalidDate = LocalDate.of(2025, 4, 7);
     String query = buildQuery(origin, destination, invalidDate);
 
     try {
-      // Act
       restTemplate.exchange(buildUrl(query), HttpMethod.GET, null, String.class);
       fail("Expected HttpClientErrorException.NotFound");
     } catch (HttpClientErrorException.NotFound ex) {
-      // Assert
       assertThat(ex.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
       assertThat(ex.getResponseBodyAsString()).contains(ErrorCode.FLIGHT_NOT_FOUND.getCode());
     }
@@ -101,11 +194,9 @@ class RouteControllerIntegrationTest {
     String query = "?originLocationId=1&destinationLocationId=2";
 
     try {
-      // Act
       restTemplate.exchange(buildUrl(query), HttpMethod.GET, null, String.class);
       fail("Expected HttpClientErrorException.BadRequest");
     } catch (HttpClientErrorException.BadRequest ex) {
-      // Assert
       assertThat(ex.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
       assertThat(ex.getResponseBodyAsString()).contains(ErrorCode.BAD_REQUEST.getCode());
       assertThat(ex.getResponseBodyAsString()).contains("Date is required");
